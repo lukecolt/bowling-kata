28f836e4d20791adbc581e2390076d000d3646c5
lukecolt <50400241+lukecolt@users.noreply.github.com>
2023-03-21 20:46:52 +0100

    Third commit

---
+++ BowlingGame.py
@@ -0,0 +1,14 @@
+class BowlingGame:
+    def __init__(self):
+        throws = []
+    
+    #def strike(self):
+
+    #def spare(self, firstThrow, secondThrow):
+
+    #def open(self, firstThrow, secondThrow):
+
+    #def roll(self, roll):
+
+    def score(self):
+        return 0
